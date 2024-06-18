0e9e8f9ccdbf4d84c2a75de4a50d1398c9e304f9
Jeanne d'Arc NYIRANWIZA <njoanc@gmail.com>
2024-06-18 18:26:29 +0200

    remove():login component from Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -19,7 +19,6 @@ const Home = () => {
 
   return (
     <div className=" flex mt-[3.13rem] flex-wrap">
-      <Link to="/login">Login</Link>
       {repos ? (
         repos.map((repo) => (
           <div
